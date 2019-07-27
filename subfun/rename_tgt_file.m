f3b3813a3939be27aa07561e8e67a8723a5d5da8
Remi Gau <remi_gau@hotmail.com>
2019-07-27 20:31:30 +0200

    make pattern for file renaming more flexible

--- subfun/rename_tgt_file.m
+++ subfun/rename_tgt_file.m
@@ -2,9 +2,11 @@ function rename_tgt_file(tgt_dir, pattern, tgt_name, ext)
 
 pattern = strrep(pattern, '-','_');
 pattern = strrep(pattern, '.','_');
+pattern = strrep(pattern, '*','.*');
 
 tgt_file = spm_select('FPList', tgt_dir, ...
-    ['^.*' pattern '.*.' ext '$']);
+    ['^.*' pattern '.*' ext '$']);
+
 movefile(tgt_file, [tgt_name '.' ext])
 
 end
