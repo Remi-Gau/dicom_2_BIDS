bdaf9f8219f479d29c5b48ceb42911665e6fe5ca
Remi Gau <remi_gau@hotmail.com>
2019-07-29 22:16:01 +0200

    Bring back the dummies!!!! We want our dummies!!!!
     - Sir, don't you dare touching our dummies!!!

---
+++ subfun/bring_back_dummies.m
@@ -0,0 +1,15 @@
+function bring_back_dummies(func_src_dir)
+
+dummy_dir = [deblank(func_src_dir) '-dummy'];
+
+dummies = spm_select('FPList', dummy_dir, ...
+    '^.*.dcm$');
+for i_dummy = 1:size(dummies,1)
+    movefile(dummies(i_dummy,:), func_src_dir)
+end
+
+pause(opt.pauseTime)
+
+rmdir(dummy_dir)
+
+end
